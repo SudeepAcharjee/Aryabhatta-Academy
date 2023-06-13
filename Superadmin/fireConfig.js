d70c0a89bc77023901fe52cfbd11758e967cab7e
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-06-14 03:10:44 +0530

    added

---
+++ Superadmin/fireConfig.js
@@ -0,0 +1,20 @@
+var firebaseConfig = {
+
+  apiKey: "AIzaSyALL1wKyNrOm2FLaOrRHgQxCoc9C-WFaVo",
+
+  authDomain: "crud-7383c.firebaseapp.com",
+
+  databaseURL: "https://crud-7383c-default-rtdb.firebaseio.com",
+
+  projectId: "crud-7383c",
+
+  storageBucket: "crud-7383c.appspot.com",
+
+  messagingSenderId: "466978073208",
+
+  appId: "1:466978073208:web:0c7a908835c55f9b655b8d"
+
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
